207c6a752c66c41ac1057c5ba1d65a7aab8858c0
Jean Menezes <jeanvbonimenezes@gmail.com>
2022-02-08 16:47:19 -0300

    Criando estrutura dos testes

---
+++ simulador.react/app-simulador/cypress/integration/Botao-simular/simular.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('quando clicar no botao simular', () => {
+    it('deve aparecer o resultado', () => {
+        // Visitar e verificar // Ver se a secao do resultado marcada esta no documento
+    });
+
+    it('deve aparecer os dados da api no resultado', () => {
+        // Clicar em tipos
+        // Ver se o valor bate com o arrange
+    })
+});
